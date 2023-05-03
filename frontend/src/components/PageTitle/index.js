378b411d9734f501ee6691e5d7657e9f766ef225
Vitaliy Buzovskiy <buzovskiy.v@gmail.com>
2023-05-03 22:59:31 +0200

    initial commit

---
+++ frontend/src/components/PageTitle/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { Container, Row, Col } from "react-bootstrap";
+import { FaHome, FaAngleRight } from "react-icons/fa";
+
+
+const PageTitle = (props) => {
+  const { t } = useTranslation();
+
+  return (
+    <section className="gauto-breadcromb-area section_70">
+      <Container>
+        <Row>
+          <Col md={12}>
+            <div className="breadcromb-box">
+              <h3>{props.pageTitle}</h3>
+              <ul>
+                <li>
+                  <FaHome />
+                </li>
+                <li>
+                  <Link to="/">{t("header-navigation.home")}</Link>
+                </li>
+                <li>
+                  <FaAngleRight />
+                </li>
+                <li>{props.pagesub}</li>
+              </ul>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default PageTitle;
