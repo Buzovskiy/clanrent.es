83a5a9efb13f03622d044d206a9879f9d09ac7ec
Buzovskyi Vitalii <buzovskiy.v@gmail.com>
2023-12-05 14:25:49 +0100

    close temporary site

---
+++ src/main-component/App/AppSiteClosed.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from "react";
+import logo from '../../img/CLAN RENT_logo_CMYK-28.png'
+import "./AppSiteClosed.scss"
+
+const AppSiteClosed = () => {
+
+   return (
+      <Fragment>
+         <img src={logo} alt=""/>
+      </Fragment>
+   );
+};
+
+export default AppSiteClosed;
